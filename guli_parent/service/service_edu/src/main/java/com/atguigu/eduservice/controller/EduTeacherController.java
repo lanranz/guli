d916085ca4effc619f156a40c8c109a2b6a8f8f2
lanranz <2377259017@qq.com>
2020-11-11 20:51:47 +0800

    updata

--- guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ guli_parent/service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -1,9 +1,17 @@
 package com.atguigu.eduservice.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -13,9 +21,50 @@ import org.springframework.web.bind.annotation.RestController;
  * @author testjava
  * @since 2020-11-10
  */
+@Api(description = "讲师管理")
 @RestController
 @RequestMapping("/eduservice/edu-teacher")
 public class EduTeacherController {
+
+    //注入service
+    @Autowired
+    private EduTeacherService teacherService;
+
+    //查询所有讲师表数据
+    @ApiOperation(value = "所有讲师列表")
+    @GetMapping("findAll")
+    public R findAllTeacher(){
+        //调用service方法来查询所有操作
+        List<EduTeacher> list = teacherService.list(null);
+        return R.ok().data("items",list);
+    }
+
+    //逻辑删除讲师表数据
+    @ApiOperation(value = "根据id删除讲师")
+    @DeleteMapping("{id}")
+    public R removeTeacher(
+            @ApiParam(name = "id",value = "讲师id",required = true)
+            @PathVariable String id){
+        boolean flag = teacherService.removeById(id);
+        if(flag){
+            return R.ok();
+        }else {
+            return R.error();
+        }
+    }
+
+    //分页查询讲师表
+    @GetMapping("pageTecher/{current}/{limit}")
+    public R pageListTeacher(@PathVariable long current,
+                             @PathVariable long limit){
+        Page<EduTeacher> pageTeacher = new Page<>(current,limit);
+        //将所有分页数据封装到pageTeacher里
+        teacherService.page(pageTeacher,null);
+        long total = pageTeacher.getTotal();//总记录数
+        List<EduTeacher> records = pageTeacher.getRecords();
+        return R.ok().data("total",total).data("rows",records);
+    }
+
 
 }
 
